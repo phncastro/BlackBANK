39bcbd88e24c60eb74496e13ab9f8bd630e53809
Pablo Henrique <phncastro3@gmail.com>
2026-01-15 06:01:36 -0300

    correction docstring

--- app.py/models/usuario.py
+++ app.py/models/usuario.py
@@ -2,14 +2,16 @@ from core.status_usuario import StatusUsuario, Enum
 from database.database import Base
 from sqlalchemy import Column, Integer, String
 from core.status_usuario import StatusUsuario
+from sqlalchemy.orm import relationship
 
 class Usuario(Base):
     ''' Classe Usuario, que representa a pessoa que deseja criar uma conta no Banco.
-        Modelo de sqlachemy '''
+        Modelo de sqlalchemy '''
 
     __tablename__ = 'usuarios'
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String)
     cpf = Column(Integer, unique= True)
     status = Column(Enum(StatusUsuario))
+    conta = relationship('Conta', back_populates= 'usuario')
 
