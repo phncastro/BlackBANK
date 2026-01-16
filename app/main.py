56abbae9488ea208f0444537248fb4bdfefd6d90
Pablo Henrique <phncastro3@gmail.com>
2026-01-16 03:50:44 -0300

    create tables in database

--- app/main.py
+++ app/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 from app.routes.usuario import usuario_router
 from app.routes.conta import conta_router
 from app.routes.banco import banco_router
+from app.database.database import Base
+from app.database.database import engine
+from app.models.conta import Conta
+from app.models.usuario import Usuario
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
