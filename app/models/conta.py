a3adbf91789039e674a1baf0645c38db81ceb53c
Pablo Henrique <phncastro3@gmail.com>
2026-01-18 04:40:59 -0300

    moving methods receber and enviar

--- app/models/conta.py
+++ app/models/conta.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, Column, ForeignKey, Float
+from sqlalchemy import Integer, Column, ForeignKey, Float, String
 from app.database.database import Base
 from sqlalchemy.orm import relationship
 
@@ -7,10 +7,37 @@ class Conta(Base):
 
     __tablename__ = 'contas'
     id = Column(Integer, primary_key=True, index=True)
-    numero = Column(Integer)
-    usuario_id = Column(Integer, ForeignKey('usuarios.id'))
-    saldo = Column(Float, default=0.0)
+    numero = Column(Integer, unique=True, nullable=False)
+    saldo = Column(Float, default=0, nullable=False)
+    usuario_id = Column(
+        Integer,
+        ForeignKey('usuarios.id'),
+        unique=True,
+        nullable=False)
     usuario = relationship('Usuario', back_populates= 'conta')
+
+    @staticmethod
+    def receber(conta_id, valor):
+        ''' Método que permite a conta alterar o valor do saldo,
+            adicionando o valor de uma transferência. 
+            
+            parâmetros:
+
+            - valor: Valor que irá ser adicionado no saldo da conta.
+        '''
+        conta_id.saldo += valor
+
+
+    @staticmethod
+    def enviar(conta_id, valor):
+        ''' Método que permite a conta alterar o valor do saldo,
+            diminuindo o valor de uma transferência. 
+            
+            parâmetros:
+
+            - valor: Valor que irá ser debitado no saldo da conta.
+        '''
+        conta_id.saldo -= valor
 
 
 
