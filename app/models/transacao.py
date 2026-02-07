60113274762a1e9f7daa19af2ca384516b95fe18
Pablo Henrique <phncastro3@gmail.com>
2026-02-06 21:41:05 -0300

    modify name

--- app/models/transacao.py
+++ app/models/transacao.py
@@ -1,12 +1,12 @@
 from sqlalchemy import Integer, Column, Float, Enum, DateTime
 from app.database.database import Base
-from app.core.transacoes import Transacoes
+from app.core.transacoes import TipoTransacao
 
 class Transacao(Base):
 
     __tablename__ = 'transacoes'
     id = Column(Integer, primary_key=True, index=True)
-    tipo = Column(Enum(Transacoes), nullable=False)
+    tipo = Column(Enum(TipoTransacao), nullable=False)
     valor = Column(Float)
     id_conta = Column(Integer)
     id_conta_destino = Column(Integer)
